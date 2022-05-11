4dc08690facba7ca625d05fa7f1fd71ff86fbc9c
Lucas-Veras <verasluca00@gmail.com>
2022-05-11 22:51:59 +0000

    changes

--- mysite/enquetes/views.py
+++ mysite/enquetes/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("DSweb project")
+    return HttpResponse(
+        "<ul><li>Nome: Lucas Oliveira Véras</li><li>Matrícula: 20211014040015</li</ul>"
+    )

bc4521d1e2b5b8d72115438e25df2d55db6bc809
Lucas-Veras <verasluca00@gmail.com>
2022-05-18 19:02:16 +0000

    update

--- mysite/enquetes/urls.py
+++ mysite/enquetes/urls.py
@@ -3,4 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('enquetes/<int:id_enquete>/', views.detalhes, name="detalhes"),
+    path(
+        'enquetes/<int:id_enquete>/votacao/',
+        views.votacao, name="votacao"
+    ),
+    path(
+        'enquetes/<int:id_enquete>/resultados/',
+        views.resultados, name="resultados"
+    ),
 ]
--- mysite/enquetes/views.py
+++ mysite/enquetes/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Pergunta
+from django.template import loader
+#from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return HttpResponse(
-        "<ul><li>Nome: Lucas Oliveira Véras</li><li>Matrícula: 20211014040015</li</ul>"
-    )
+    ultima_questao_lista = Pergunta.objects.order_by("data_pub")[:5]
+    template = loader.get_template('enquetes/index.html')
+    contexto = {
+            'ultima_questao_lista' : ultima_questao_lista,
+        }
+    return HttpResponse(template.render(contexto, request))
+
+def detalhes(resquest, id_enquete):
+    resposta = "você está visualizando a questão %s."
+    return HttpResponse(resposta % id_enquete)
+
+def resultados(request, id_enquete):
+    resposta = "Deltahes da enquete #$s."
+    return HttpResponse(resposta % id_enquete)
+
+def votacao(resquest, id_enquete):
+    resposta = "Votação para a questão %s."
+    return HttpResponse(resposta % id_enquete)
+

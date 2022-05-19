5732195cef9e8f6bf0def99b257a146573c8a63e
Lucas-Veras <verasluca00@gmail.com>
2022-05-19 20:24:11 -0300

    HTML update

--- mysite/enquetes/views.py
+++ mysite/enquetes/views.py
@@ -5,14 +5,14 @@ from django.template import loader
 # Create your views here.
 
 def index(request):
-    ultima_questao_lista = Pergunta.objects.order_by("data_pub")[:5]
+    questao_lista = Pergunta.objects.order_by("data_pub")[:5]
     template = loader.get_template('enquetes/index.html')
     contexto = {
-            'ultima_questao_lista' : ultima_questao_lista,
+            'questao_lista' : questao_lista,
         }
     return HttpResponse(template.render(contexto, request))
 
-def detalhes(resquest, id_enquete):
+def detalhes(request, id_enquete):
     resposta = "você está visualizando a questão %s."
     return HttpResponse(resposta % id_enquete)
 
@@ -20,7 +20,7 @@ def resultados(request, id_enquete):
     resposta = "Deltahes da enquete #$s."
     return HttpResponse(resposta % id_enquete)
 
-def votacao(resquest, id_enquete):
+def votacao(request, id_enquete):
     resposta = "Votação para a questão %s."
     return HttpResponse(resposta % id_enquete)
 

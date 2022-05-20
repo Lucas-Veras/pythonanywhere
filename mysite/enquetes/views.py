a031f19bacf1a8fde0fb1d06497b558ee699a216
Lucas-Veras <verasluca00@gmail.com>
2022-05-20 00:24:24 +0000

    update

--- mysite/enquetes/views.py
+++ mysite/enquetes/views.py
@@ -5,16 +5,6 @@ from django.template import loader
 # Create your views here.
 
 def index(request):
-<<<<<<< HEAD
-    ultima_questao_lista = Pergunta.objects.order_by("data_pub")[:5]
-    template = loader.get_template('enquetes/index.html')
-    contexto = {
-            'ultima_questao_lista' : ultima_questao_lista,
-        }
-    return HttpResponse(template.render(contexto, request))
-
-def detalhes(resquest, id_enquete):
-=======
     questao_lista = Pergunta.objects.order_by("data_pub")[:5]
     template = loader.get_template('enquetes/index.html')
     contexto = {
@@ -23,7 +13,6 @@ def detalhes(resquest, id_enquete):
     return HttpResponse(template.render(contexto, request))
 
 def detalhes(request, id_enquete):
->>>>>>> 5732195cef9e8f6bf0def99b257a146573c8a63e
     resposta = "você está visualizando a questão %s."
     return HttpResponse(resposta % id_enquete)
 
@@ -31,11 +20,7 @@ def resultados(request, id_enquete):
     resposta = "Deltahes da enquete #$s."
     return HttpResponse(resposta % id_enquete)
 
-<<<<<<< HEAD
-def votacao(resquest, id_enquete):
-=======
 def votacao(request, id_enquete):
->>>>>>> 5732195cef9e8f6bf0def99b257a146573c8a63e
     resposta = "Votação para a questão %s."
     return HttpResponse(resposta % id_enquete)
 

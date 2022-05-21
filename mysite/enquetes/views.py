02b779ef3f9989fa8133caff27dd86b2cf7da6fa
Lucas-Veras <verasluca00@gmail.com>
2022-05-21 19:17:19 +0000

    update

--- mysite/enquetes/views.py
+++ mysite/enquetes/views.py
@@ -1,20 +1,26 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from .models import Pergunta
-from django.template import loader
-#from django.shortcuts import render
+#from django.template import loader
+from django.shortcuts import render
+
 # Create your views here.
 
 def index(request):
     questao_lista = Pergunta.objects.order_by("data_pub")[:5]
-    template = loader.get_template('enquetes/index.html')
+ #   template = loader.get_template('enquetes/index.html')
     contexto = {
             'questao_lista' : questao_lista,
         }
-    return HttpResponse(template.render(contexto, request))
+    return render(request, 'enquetes/index.html', contexto)
+  #  return HttpResponse(template.render(contexto, request))
 
-def detalhes(request, id_enquete):
-    resposta = "você está visualizando a questão %s."
-    return HttpResponse(resposta % id_enquete)
+def detalhes(request, id_pergunta):
+    try:
+        pergunta = Pergunta.objects.get(pk = id_pergunta)
+    except Pergunta.DoesNotExist:
+        raise Http404("Enquete desejada não existe")
+        contexto = { 'pergunta' : pergunta }
+    return render(request, 'enquetes/detalhes', contexto)
 
 def resultados(request, id_enquete):
     resposta = "Deltahes da enquete #$s."
